cb3ac069a91a99ca74928c0af9a2b50358349ec9
Siddharth Kushal <kushal.s@northeastern.edu>
2024-07-16 21:17:12 -0400

    tic-tac-toe game

---
+++ tic-tac-toe/src/components/tic-tac-toe.jsx
@@ -0,0 +1,36 @@
+import { useState } from "react";
+import useTictacToe from "../hooks/use-tic-tac-toe";
+
+function TicTacToe() {
+  const { board, handleClick, calculateWinner, getStatusMessage, resetGame } =
+    useTictacToe();
+
+  console.log(board);
+  return (
+    <div className="game">
+      <div className="status">
+        {getStatusMessage()}
+        <button className="reset" onClick={resetGame}>
+          Reset
+        </button>
+      </div>
+
+      <div className="board">
+        {board.map((b, index) => {
+          return (
+            <button
+              className="cell"
+              key={index}
+              onClick={() => handleClick(index)}
+              disabled={b !== null}
+            >
+              {b}
+            </button>
+          );
+        })}
+      </div>
+    </div>
+  );
+}
+
+export default TicTacToe;
